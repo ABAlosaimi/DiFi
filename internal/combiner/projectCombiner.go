1e18fbc1671eb3ca974cf1f9618b67a20686db5d
ABAlosaimi <afalih.alosaimi@gmail.com>
2026-01-13 03:14:08 +0300

    added the prject strcuture and some code for 1st feature

--- internal/combiner/projectCombiner.go
+++ internal/combiner/projectCombiner.go
@@ -1,2 +1,16 @@
 package combiner
 
+import (
+	"fmt"
+	"os"
+)
+
+func addPreviousFeature(featureFileName string, featureDirName string, projectDir string, projectSubDir string) bool {
+    file, err := os.Open(featureDirName + "/" + featureFileName)
+    if err != nil {
+        return false
+    }
+    defer file.Close()
+
+    return true
+}
