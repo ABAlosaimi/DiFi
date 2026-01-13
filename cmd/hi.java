07e0d568fe16f6621cbfb20a51b4248202e428ab
ABAlosaimi <afalih.alosaimi@gmail.com>
2026-01-14 02:39:35 +0300

    tested the basic opeing of file and filtering

---
+++ cmd/hi.java
@@ -0,0 +1,10 @@
+package main;
+
+import System.out.println;
+import Date;
+
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
--- internal/combiner/projectCombiner.go
+++ internal/combiner/projectCombiner.go
@@ -1,16 +1,57 @@
 package combiner
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
-func addPreviousFeature(featureFileName string, featureDirName string, projectDir string, projectSubDir string) bool {
-    file, err := os.Open(featureDirName + "/" + featureFileName)
-    if err != nil {
-        return false
-    }
-    defer file.Close()
+func OpenFeatureFile(filePath string) os.File {
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("opening file error: " + err.Error())
+	}
+	defer file.Close()
 
-    return true
+	return *file
+}
+
+func removeNlines(featureFile []string, num int) *os.File {
+	 newFeatureFile, err := os.Create("x.java")
+	if err != nil {
+		fmt.Println("file creation error: " + err.Error())
+	}
+
+	for i := num; i < len(featureFile); i++ {
+		_, err := newFeatureFile.Write([]byte(featureFile[i]))
+		if err != nil {
+			fmt.Println("somthing went wrong with the filteration process: " + err.Error())
+			break
+		}
+	}
+
+	return newFeatureFile
+}
+
+func FilterFeatureFile(featureFile os.File, filteringStartNum int) *os.File {
+	var featureFileArray []string
+	scanner := bufio.NewScanner(&featureFile)
+
+	for scanner.Scan() {
+		featureFileArray = append(featureFileArray, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+        fmt.Println("Error reading file:", err)
+    }
+
+	filteredFile := removeNlines(featureFileArray, filteringStartNum)
+
+	return filteredFile
+}
+
+func AddFeatureFile(featureFile os.File, projectDir string) bool {
+	
+	// adding the filtered file to the new prject diroctory 
+	return true
 }
